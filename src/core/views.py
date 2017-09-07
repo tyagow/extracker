94f2229e87944e70852cdd1872692ecf967e76ee
Tiago Almeida <tyagow@hotmail.com.br>
2017-09-07 19:02:33 -0300

    bids operation working

--- src/core/views.py
+++ src/core/views.py
@@ -7,10 +7,11 @@ def home(request):
 
 
 def valid_value(param, operation, value):
+    print(param)
     if 'asks' in operation:
         return int(value) >= param[0]
     if 'bids' in operation:
-        return int(value) < param[0]
+        return int(value) < param[1]
 
 
 def deep(request, operation, value):
@@ -20,14 +21,26 @@ def deep(request, operation, value):
         data = json.loads(url.read().decode())
 
     index = 0
-    btc_total = 0.0
+    total = 0.0
+    operation_index = 1 if 'asks' in operation else 0
+    total_btc = 0
     while True:
-        if valid_value(data[operation][index], operation, value):
-            btc_total += data[operation][index][1]
-            print(data[operation][index][0], value, btc_total)
-            index +=1
+        if 'asks' in operation and int(value) >= data[operation][index][0]:
+            total += data[operation][index][1]
+            print(data[operation][index][0], value, total)
+            index += 1
+        elif 'bids' in operation:
+
+            if int(value) > total_btc:
+                total += data[operation][index][0] * data[operation][index][1]
+                total_btc += data[operation][index][1]
+                print(data[operation][index][1], value, total, total_btc)
+                index += 1
+            else:
+                total = total / float(total_btc)
+                operation = ''
+
         else:
-            return HttpResponse(btc_total)
-    return HttpResponse(data[operation])
+            return HttpResponse(total)
 
 
